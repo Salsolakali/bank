729bc5d19dada348995d6a7445266d339072c4f2
Alberto Fernandez Seral <alberto.fernandezseral@unisys.com>
2019-10-22 10:36:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bank/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.bank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
